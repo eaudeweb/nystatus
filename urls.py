561df44a47855f78bc7b8145c1b1a29aa8f4f5ab
Eau de Web <eaudeweb@Dragoss-MacBook-Air.local>
2012-02-08 15:57:52 +0200

    updated gitignore, added HOST in settings_local and remove SITE_PATH from urls

--- urls.py
+++ urls.py
@@ -1,28 +1,21 @@
 from django.conf.urls.defaults import *
 import django.views.generic.simple
-from settings import SITE_PATH, MEDIA_ROOT
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^src/', include('src.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+urlpatterns = patterns("",
+    # Uncomment the admin/doc line below and add "django.contrib.admindocs"
     # to INSTALLED_APPS to enable admin documentation:
-    (r'^' + SITE_PATH + 'admin/doc/',
-                             include('django.contrib.admindocs.urls')),
+    (r"^admin/doc/", include("django.contrib.admindocs.urls")),
 
     # Uncomment the next line to enable the admin:
-    (r'^' + SITE_PATH + 'admin/nystatus/zopeinstance/check/(?P<id>\d+)/$', 'nystatus.views.admin_trigger'),
-    (r'^' + SITE_PATH + 'admin/nystatus/product/check/(?P<id>\d+)/$', 'nystatus.views.admin_product_trigger'),
-    (r'^' + SITE_PATH + 'admin/', include(admin.site.urls)),
-    (r'^' + SITE_PATH+ 'media/(.*)', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
-    (r'^' + SITE_PATH + '$', 'nystatus.views.index')
+    (r"^admin/nystatus/zopeinstance/check/(?P<id>\d+)/$", "nystatus.views.admin_trigger"),
+    (r"^admin/nystatus/product/check/(?P<id>\d+)/$", "nystatus.views.admin_product_trigger"),
+    (r"^admin/", include(admin.site.urls)),
+    (r"^media/(.*)", "django.views.static.serve", {"document_root": settings.MEDIA_ROOT}),
+    (r"^$", "nystatus.views.index")
 )
 
-#urlpatterns += patterns('django.views.generic.simple',
-#    ('^' + SITE_PATH + '/?$', 'redirect_to', {'url': 'admin/'})
-#)
